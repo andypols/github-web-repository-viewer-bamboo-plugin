86c73a8cf7aa21f262135c1ae21575424f973e78
Andy <andy@Andys-MacBook-Pro.local>
2011-02-20 01:21:01 +0000

    More tests

--- src/test/java/uk/co/pols/bamboo/githubplugin/GitHubWebRepositoryViewerTest.java
+++ src/test/java/uk/co/pols/bamboo/githubplugin/GitHubWebRepositoryViewerTest.java
@@ -1,8 +1,14 @@
 package uk.co.pols.bamboo.githubplugin;
 
+import com.atlassian.bamboo.plugins.git.GitRepository;
+import com.atlassian.bamboo.utils.error.ErrorCollection;
+import com.atlassian.bamboo.ww2.actions.build.admin.create.BuildConfiguration;
 import org.jmock.integration.junit3.MockObjectTestCase;
 
 import java.util.Arrays;
+
+import static uk.co.pols.bamboo.githubplugin.GitHubWebRepositoryViewer.WEB_REPO_URL;
+import static uk.co.pols.bamboo.githubplugin.SampleCommitFactory.commitWithFile;
 
 public class GitHubWebRepositoryViewerTest extends MockObjectTestCase {
     private static final String WEB_REPO_FOR_THIS_PROJECT = "https://github.com/andypols/github-web-repository-viewer-bamboo-plugin";
@@ -19,10 +25,10 @@ public class GitHubWebRepositoryViewerTest extends MockObjectTestCase {
         assertEquals(WEB_REPO_FOR_THIS_PROJECT, gitHubViewer.getWebRepositoryUrl());
     }
 
-//    public void testProvidesBambooWithWebUrlAllowingTheCodeChangePageLinkBackToGitHub() {
-//        gitHubViewer.setWebRepositoryUrl("https://github.com/andypols/git-bamboo-plugin");
-//
-//        assertEquals("https://github.com/andypols/git-bamboo-plugin/commit/71b2bf41fb82a12ca3d4d34bd62568d9167dc6d6", gitHubViewer.getWebRepositoryUrlForCommit(commitWithFile("71b2bf41fb82a12ca3d4d34bd62568d9167dc6d6"), null));
-//        assertEquals("https://github.com/andypols/git-bamboo-plugin/blob/71b2bf41fb82a12ca3d4d34bd62568d9167dc6d6/src/main/java/uk/co/pols/bamboo/gitplugin/GitRepository.java", gitHubViewer.getWebRepositoryUrlForFile(commitFile("71b2bf41fb82a12ca3d4d34bd62568d9167dc6d6")));
-//    }
+    public void testProvidesBambooWithWebUrlAllowingTheCodeChangePageLinkBackToGitHub() {
+        gitHubViewer.setWebRepositoryUrl("https://github.com/andypols/git-bamboo-plugin");
+
+        assertEquals("https://github.com/andypols/git-bamboo-plugin/commit/71b2bf41fb82a12ca3d4d34bd62568d9167dc6d6", gitHubViewer.getWebRepositoryUrlForCommit(commitWithFile("71b2bf41fb82a12ca3d4d34bd62568d9167dc6d6"), null));
+//        assertEquals("https://github.com/andypols/git-bamboo-plugin/blob/71b2bf41fb82a12ca3d4d34bd62568d9167dc6d6/src/main/java/uk/co/pols/bamboo/gitplugin/GitRepository.java", gitHubViewer.getWebRepositoryUrlForFile(SampleCommitFactory.commitFile("71b2bf41fb82a12ca3d4d34bd62568d9167dc6d6")));
+    }
 }
